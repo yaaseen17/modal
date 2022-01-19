a71729382c73212911cb29dbf30a0d05b708b287
Yaaseen Davids <yaaseendav@gmail.com>
2022-01-19 14:28:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: `first`,
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: red;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <h1>hoyah</h1>
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: rgb(0, 0, 0);
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: transparent;
+  width: 100%;
+  height: 100%;
+}
+h1 {
+  color: rgb(92, 255, 71);
+}
+</style>
